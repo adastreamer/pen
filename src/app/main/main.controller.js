97a4437d3b433ea7476770deffc2212f403c9a46
Eugene Khashin <adastreamer@gmail.com>
2016-04-29 14:18:38 +0300

    comments editdone

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -20,11 +20,20 @@
 
       var pin = {
         x: x,
-        y: y
+        y: y,
+        comments: []
       };
 
       vm.pins.push(pin);
       vm.selectPin(pin);
+      vm.newmessage = "";
+    };
+
+    vm.removePin = function(pin){
+      if (!confirm("Are you sure?!")) return;
+      vm.pins = vm.pins.filter(function(p){
+        return p != pin;
+      });
     };
 
     vm.clearSelectedPin = function(){
@@ -33,6 +42,52 @@
 
     vm.selectPin = function(pin){
       vm.selectedPin = pin;
+      vm.scrollCommentsToBottom();
+    };
+
+
+    vm.newmessage = "";
+    vm.addCommentToPin = function(pin, message, username){
+      if (!message){
+        return;
+      }
+      pin.comments.push({
+        message: message,
+        username: username
+      });
+      vm.newmessage = "";
+      vm.scrollCommentsToBottom();
+    };
+
+    vm.removeCommentFromPin = function(pin, comment){
+      if (!confirm("Are you sure?!")) return;
+      pin.comments = pin.comments.filter(function(c){
+        return c != comment;
+      });
+    };
+
+    vm.scrollCommentsToBottom = function(){
+      setTimeout(function(){
+        var commentsLists = document.getElementsByClassName("comments-list");
+        for (var i = 0; i < commentsLists.length; i++){
+          commentsLists[i].scrollTop = commentsLists[i].scrollHeight;
+        }        
+      })
+    };
+
+
+    vm.startEdit = function(comment){
+      comment.newMessage = comment.message;
+      comment.isInEditMode = true;
+    };
+    vm.cancelEdit = function(comment){
+      comment.newMessage = "";
+      comment.isInEditMode = false;
+    };
+    vm.saveEdit = function(comment){
+      comment.message = comment.newMessage;
+      comment.newMessage = "";
+      comment.isInEditMode = false;
     };
   }
 })();
