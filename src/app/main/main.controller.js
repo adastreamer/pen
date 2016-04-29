d70c06ae774f70a2b9488664cf1ad4b0c9ecdcac
Eugene Khashin <adastreamer@gmail.com>
2016-04-29 13:25:16 +0300

    pincreation done

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,6 +8,31 @@
   /** @ngInject */
   function MainController($timeout) {
     var vm = this;
+    var pinOffset = 7;
+    vm.username = '@adastreamer';
 
+    vm.pins = [];
+
+    vm.addPin = function(event){
+      var bounds = event.target.getBoundingClientRect();
+      var x = event.clientX - bounds.left - pinOffset;
+      var y = event.clientY - bounds.top - pinOffset;
+
+      var pin = {
+        x: x,
+        y: y
+      };
+
+      vm.pins.push(pin);
+      vm.selectPin(pin);
+    };
+
+    vm.clearSelectedPin = function(){
+      vm.selectedPin = null;
+    };
+
+    vm.selectPin = function(pin){
+      vm.selectedPin = pin;
+    };
   }
 })();
